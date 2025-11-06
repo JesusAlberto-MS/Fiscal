8817f71cb280cf5e03b6504b1f6934435e7fdbd6
Ivan <iver@users.noreply.github.com>
2025-11-06 13:57:22 -0600

    Se agregan dependencias de calidad (#4)
    * Se agrega módulo se supervición para tolerancia a fallos
    
    * Add quality deps
    
    * Formateando Correctamente y Regex Corregida
    
    * Agregando archivo .envrc
    
    ---------
    
    Co-authored-by: JesusAlberto-MS <jesus@romikya.mx>

--- mix.exs
+++ mix.exs
@@ -9,22 +9,38 @@ defmodule Fiscal.MixProject do
       version: @version,
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.cobertura": :test
+      ]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Fiscal.Application, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # Dependencias de análisis de código y calidad
+      {:credo, ">= 0.0.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, ">= 0.0.0", only: [:dev, :test], runtime: false},
+      {:doctor, ">= 0.0.0", only: [:dev, :test], runtime: false},
+      {:ex_check, "~> 0.16.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: [:dev, :test], runtime: false},
+      {:ex_machina, "~> 2.8.0", only: :test},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:mix_audit, ">= 0.0.0", only: [:dev, :test], runtime: false}
     ]
   end
 end
