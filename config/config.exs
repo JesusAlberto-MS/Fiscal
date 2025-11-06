0b3d9e3376177f468fd0ff60186888de865ec116
JesusAlberto-MS <jesus@romikya.mx>
2025-11-05 19:21:38 -0600

    Agregar configuración del cliente HTTP y credenciales de la API

--- config/config.exs
+++ config/config.exs
@@ -3,5 +3,14 @@ import Config
 config :fiscal,
   env: config_env()
 
+# Configuración del cliente HTTP
+config :fiscal, Fiscal.Http, client: Fiscal.Http.HttpClient
+
+# Credenciales de la API
+config :fiscal,
+  api_key: System.get_env("FISCAL_API_KEY_TEST"),
+  tenant_key: System.get_env("FISCAL_TENANT_KEY_TEST"),
+  base_url: System.get_env("FISCAL_ULR_TEST")
+
 # Import environment-specific configuration
 import_config "#{config_env()}.exs"
